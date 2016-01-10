33677873550ff3cd46481d3efe6b50ada894e128
Kevin Eddye <kevneddy@gmail.com>
2016-01-10 15:25:09 -0800

    Add routes and oAuth to LinkedIn

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.locals.users = req.user || "";
+	console.log(req.user);
+  	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function ensureAuthenticated(req, res, next) {
+	if(req.isAuthenticated()) { return next();}
+  	res.send('/login');
+})
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next){
+	res.send('respond with a resource');
 });
 
 module.exports = router;
